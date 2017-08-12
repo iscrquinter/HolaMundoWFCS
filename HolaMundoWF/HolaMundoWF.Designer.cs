96f06a5743f3fc83303cec9165587640f85a830f
Ricardo R Quintero Meza <iscrquinter@outlook.com>
2017-08-12 12:07:26 -0600

    Ajustes finales ...

--- HolaMundoWF/HolaMundoWF.Designer.cs
+++ HolaMundoWF/HolaMundoWF.Designer.cs
@@ -29,6 +29,7 @@
         private void InitializeComponent()
         {
             this.msg = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // msg
@@ -39,11 +40,22 @@
             this.msg.Size = new System.Drawing.Size(0, 13);
             this.msg.TabIndex = 0;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(105, 151);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Saludar!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // HolaMundoWF
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.msg);
             this.Name = "HolaMundoWF";
             this.Text = "HolaMundo!";
@@ -55,6 +67,7 @@
         #endregion
 
         private System.Windows.Forms.Label msg;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- HolaMundoWF/HolaMundoWF.cs
+++ HolaMundoWF/HolaMundoWF.cs
@@ -16,5 +16,10 @@ namespace HolaMundoWF
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            msg.Text = "Hola Mundo!";
+        }
     }
 }
